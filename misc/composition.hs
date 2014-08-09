bb29037b4c2b305553d3ed5c708c2a69f06929d6
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2014-08-09 15:53:41 +0530

    adding some comments

--- misc/composition.hs
+++ misc/composition.hs
@@ -3,9 +3,11 @@
 -- (f . g) x = f (g x) Applies function f after applying 
 -- function g on x
 
+-- returns the square of the number
 squares :: Int -> Int
 squares x = x * x
 
+-- checks if a number is positive
 positive :: Int -> Bool
 positive x = x > 0
 
